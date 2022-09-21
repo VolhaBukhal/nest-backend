b4aae4d166154e1db983672838a288dc82a12d23
volha <o.bukhal@gmail.com>
2022-09-21 11:44:35 +0300

    init: start nest with main structure

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  controllers: [AppController],
+  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1111',
+      database: 'nest-course',
+      models: [],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
